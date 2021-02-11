a03e2689c9d7d44b94d97ac8046df37c260cdbe3
Kent Jakobsson <kentegel@gmail.com>
2021-02-11 11:23:39 +0100

    2

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
 
-class Quiz extends Component{
+class Quiz extends Component {
   constructor(props) {
     super(props)
     this.state = { quiz_position: 1 }
   }
   render() {
-    return(
+    return (
       <div>
         <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
       </div>
-      )
+    )
   }
 }
 
