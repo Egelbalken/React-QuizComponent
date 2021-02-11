a773fadafb5cb8b623a638b70ae1a2e070932523
Kent Jakobsson <kentegel@gmail.com>
2021-02-11 10:33:29 +0100

    Create Quiz.js
    First build test.

---
+++ src/Quiz.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+let quizData = required('./quiz_data.json')
+
+class Quiz extends React.Component{
+  render() {
+    <div className="QuizQuestion"></div>
+
+    constructor(props){
+      super(props);
+      this.state = {quiz_position: 1}
+    }
+
+  }
+}
+
+export default Quiz
