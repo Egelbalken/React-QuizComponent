cb04d2458b4155f997c5513495682443f84a4a52
Kent Jakobsson <kentegel@gmail.com>
2021-02-11 14:30:34 +0100

    firstatempt

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import QuizQuestion from './QuizQuestion.js'
 
+import QuizEnd from './QuizEnd'
+
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
